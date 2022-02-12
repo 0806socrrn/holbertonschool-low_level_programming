2cc1adb7c176586044f93dc2e1cf4bba9813b0a9
0806socrrn <socrrn@gmail.com>
2022-02-11 21:21:19 -0500

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - numbers of base 10 starting from 0
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = '0'; c <= '9'; c++)
+		putchar(c);
+		putchar('\n');
+
+	return (0);
+}

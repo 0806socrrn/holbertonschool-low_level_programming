f766c2df9f78a4eba1b42f3249b09d78bd5108ae
0806socrrn <socrrn@gmail.com>
2022-02-11 21:05:27 -0500

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - numbers of base 10
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0; c < 10; c++)
+		printf("%d", c);
+		printf("\n");
+
+	return (0);
+}

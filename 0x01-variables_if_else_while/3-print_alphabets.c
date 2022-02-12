b2caee22186135fbd700214b8d4425163883c372
0806socrrn <socrrn@gmail.com>
2022-02-11 20:42:17 -0500

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet in lowercase.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+	char b = 'a';
+        char c = 'A';
+	
+	while (b <= 'z')
+	{
+		putchar(b);
+		b++;
+	}
+        putchar('\n');
+
+	while (c <= 'Z')
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

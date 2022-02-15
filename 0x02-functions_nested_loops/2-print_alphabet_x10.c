f9061cd3a38606b4802404e7bffcb654d59ec7e5
0806socrrn <socrrn@gmail.com>
2022-02-15 17:33:56 -0500

    2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: Always 0 
+ */
+void print_alphabet(void)
+{
+	char y;
+        int j;
+	for (j=10; j > 0 ;j++)
+	{
+	     for (y = 'a'; y <= 'z'; y++)
+		_putchar(y);
+	_putchar('\n');
+	    }
+}

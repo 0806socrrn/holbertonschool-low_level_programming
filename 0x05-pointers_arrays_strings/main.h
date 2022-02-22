528fbcc6ea61ca2e31f0fbc0e82588e17f77398f
0806socrrn <socrrn@gmail.com>
2022-02-22 18:31:05 -0500

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,21 @@
+#ifndef MAIN_TYPE
+#define MAIN_TYPE
+
+/*LITBS*/
+#include <unistd.h>
+#include <stdio.h>
+
+/*PROTOTYPES*/
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *src);
+
+#endif/*MAIN_TYPE*/

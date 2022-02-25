f8434db781cb2bba387d951a2883fa26394c521a
0806socrrn <socrrn@gmail.com>
2022-02-24 19:37:36 -0500

    0. strcat

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_TYPE
+#define MAIN_TYPE
+
+/*LITBS*/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/*PROTOTYPES*/
+
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+
+#endif/*MAIN_TYPE*/

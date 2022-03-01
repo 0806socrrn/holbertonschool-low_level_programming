3c7a37f5e71bdfcfe2b4156bb718d88551d75ef2
0806socrrn <socrrn@gmail.com>
2022-02-28 19:51:38 -0500

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strchr - searches string for a character
+ * @s: pointer to beginning of string to be searched
+ * @c: character to be searched for
+ * Return: pointer to character if found, otherwise returns NULL
+ */
+char *_strchr(char *s, char c)
+{
+for (; *s && *s != c; s++)
+
+if (*s == c)
+{
+return (s);
+}
+else
+{
+return (NULL);
+}
+
+}
+

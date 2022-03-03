35c359cddbb05b6afa8baa681ec94c728f0674bb
0806socrrn <socrrn@gmail.com>
2022-03-02 21:02:55 -0500

    5. Your subconscious is looking for the dreamer

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ *  _sqrt - main functioin
+ * @y: 1 vari
+ * @x: 2 vari
+ * Return: 0
+ */
+
+int _sqrt(int x, int y)
+{
+int sqrt = y * y;
+
+if (sqrt > x)
+return (-1);
+if (sqrt == x)
+return (y);
+
+return (_sqrt(x, y + 1));
+}
+/**
+ * _sqrt_recursion - 2nd function
+ * @n: 3 vari
+ * Return: 0
+ */
+
+int _sqrt_recursion(int n)
+{
+return (_sqrt(n, 0));
+}

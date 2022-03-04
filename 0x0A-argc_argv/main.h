f1bbbd2b137ba0604356e49edbb70e27777106d8
0806socrrn <socrrn@gmail.com>
2022-03-04 11:14:34 -0500

    0. It ain't what they call you

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_TYPE
+#define MAIN_TYPE
+
+/*LITBS*/
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+
+/*PROTOTYPES*/
+
+int _putchar(char c);
+long int atoi(char *);
+
+#endif/*MAIN_TYPE*/

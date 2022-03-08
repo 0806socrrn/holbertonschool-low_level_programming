75e5a33460b681bf71c9ea0c429f5c5d141fd606
0806socrrn <socrrn@gmail.com>
2022-03-08 17:17:56 -0500

    correction

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,7 +1,6 @@
-#ifndef MAIN_TYPE
-#define MAIN_TYPE
+#ifndef MAIN_H_
+#define MAIN_H_
 
-/*PROTOTYPES*/
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -22,5 +21,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif/*MAIN_TYPE*/
 
+#endif

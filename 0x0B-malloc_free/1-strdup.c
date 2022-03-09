d31d5adea9a4c23ad65974663b563226621e2fae
0806socrrn <socrrn@gmail.com>
2022-03-08 21:04:05 -0500

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,56 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strlen - swap the values of two integers
+ * @s: pointer to a char
+ * Return: number of chars
+ */
+int _strlen(char *s)
+{
+	int acc = 0;
+
+	while (s[acc] != '\0')
+	{
+		acc++;
+	}
+	return (acc);
+}
+
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as a parameter.
+ * @str: Array of chars
+ *
+ * Return: NULL if string is NULL or error
+ * otherwise pointer to the duped arr
+ */
+char *_strdup(char *str)
+{
+	int i;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		int len = _strlen(str);
+		char *out = malloc(sizeof(char) * (len + 1));
+
+		if (out == NULL)
+		{
+			return (NULL);
+		}
+
+		for (i = 0; i < len; i++)
+		{
+			*(out + i) = *(str + i);
+		}
+		*(out + i) = '\0';
+
+		return (out);
+	}
+}

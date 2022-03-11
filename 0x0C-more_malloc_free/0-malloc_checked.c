9ddf1a98046b5a097ba9fe8b20a9eee7a5ba8034
0806socrrn <socrrn@gmail.com>
2022-03-11 14:06:30 -0500

    More malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory
+ * @b: Number of bytes of memory to be allocated
+ *
+ * Description: Allocates `b` bytes of memory, but will exit with status `98`
+ *              if there is an error
+ *
+ * Return: Pointer to the first address in the newly allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+
+	return (mem);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+Task malloc and free

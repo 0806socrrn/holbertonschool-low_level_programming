e9e6547452f55747f2d33f94e0902af3e3fa04a9
0806socrrn <socrrn@gmail.com>
2022-03-17 18:25:17 -0500

    Function pointers

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,33 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Perform an operation on 2 numbers.
+ * @argc: Number of arguments.
+ * @argv: Arguments vector.
+ * Return: 0 .
+ **/
+
+int main(int argc, char *argv[])
+{
+int a, b, e;
+int (*f)(int, int);
+
+if (argc != 4)
+{
+printf("Error\n");
+exit(98);
+}
+a = atoi(argv[1]);
+b = atoi(argv[3]);
+f = get_op_func(argv[2]);
+if (f == NULL)
+{
+printf("Error\n");
+exit(99);
+}
+e = f(a, b);
+printf("%d\n", e);
+return (0);
+}

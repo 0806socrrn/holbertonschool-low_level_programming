23b94b993342db32ba06ec3ae66da99fca772b7e
0806socrrn <socrrn@gmail.com>
2022-04-04 18:31:57 -0500

    Bit manipulation

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_binary - prints the binary representation of a number
+ * @n: the number to be scanned
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int in = 0;
+	unsigned long int num = n;
+
+	while ((num >>= 1) > 0)
+		in++;
+	while (in >= 0 )
+	{
+		putchar((n >> in) & 1 ? '1' : '0');
+		in--;
+	}
+}

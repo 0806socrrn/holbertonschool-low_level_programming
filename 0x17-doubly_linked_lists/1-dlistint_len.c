ed7837b8b0c5e01ca69a529219c36abc81183233
0806socrrn <socrrn@gmail.com>
2022-05-12 10:37:24 -0500

    Doubly linked lists

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,18 +1,15 @@
 #include "lists.h"
+
 /**
- * dlistint_len - return number of elements in a linked list
- * @h: pointer
- * Return: nunmer of elements
+ * dlistint_len - Prints a doubly linked list in order
+ * @h: The head of the list
+ * Return: The number of nodes in the doubly linked list
  */
 size_t dlistint_len(const dlistint_t *h)
 {
-
-size_t j = 0;
-
-while (h)
-{
-j++;
-h = h->next;
-}
-return (j);
+size_t count = 0;
+const dlistint_t *node = h;
+for (; node; node = node->next, ++count)
+;
+return (count);
 }

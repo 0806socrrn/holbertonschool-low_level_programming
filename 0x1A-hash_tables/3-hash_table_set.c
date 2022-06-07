d49410111204160cf635885979ea0e7617a782bd
0806socrrn <socrrn@gmail.com>
2022-06-06 20:47:13 -0500

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,36 @@
-#include"hash_tables.h"
+#include "hash_tables.h"
 /**
- * hash_table_set - add item to the table
- * @ht: table entry
- * @key: table key
- * @value: key value
- * Return: 1 on success 0 otherwise
+ *hash_table_set - adds an element to the hash table
+ *@ht: pointer to the hash
+ *@key: pointer to the key
+ *@value: value to add
+ *Return: 1 if is success or 0
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-hash_node_t *elem = NULL;
-char *value = NULL;
-unsigned long int index = 0,
-i = 0;
-if (!ht || !key || !*key || value == NULL)
+unsigned long index, size;
+hash_node_t *new_node;
+
+if (ht  == NULL || key == NULL || value == NULL)
 return (0);
-index = key_index((const unsigned char *)key, ht->size);
-value = strdup(value);
-if (!value)
-return (0);
-for (i = index; ht->array[i]; i++)
+
+size = ht->size;
+index = key_index((const unsigned char *)key, size);
+
+if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 {
-if (strcmp(ht->array[i]->key, key) == 0)
-{
-free(ht->array[i]->value);
-ht->array[i]->value = value;
+ht->array[index]->value = strdup(value);
 return (1);
 }
-}
 
-elem = malloc(sizeof(hash_node_t));
-if (!elem)
-{
-free(value);
-return (0);
-}
-elem->key = strdup(key);
-if (!elem->key)
-{
-free(elem);
-return (0);
-}
-elem->value = value;
-elem->next = ht->array[index];
-ht->array[index] = elem;
+new_node = malloc(sizeof(hash_node_t));
 
+if (new_node == NULL)
+return (0);
+
+new_node->key = strdup(key);
+new_node->value = strdup(value);
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
 return (1);
 }
